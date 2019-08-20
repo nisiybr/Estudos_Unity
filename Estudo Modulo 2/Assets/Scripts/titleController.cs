c0647a9a00054f3eff353592f8f897d6f60321e0
nisiybr <guilherme.nisiyama@gmail.com>
2019-08-19 22:46:43 -0300

    Aula 35 e 36
    Multi Idioma - XML

---
+++ Estudo Modulo 2/Assets/Scripts/titleController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class titleController : MonoBehaviour {
+
+	public Text txtInstrucoes;
+
+	private loadXMLFile loadXMLFile;
+	
+	// Use this for initialization
+	void Start () {
+		loadXMLFile = FindObjectOfType(typeof(loadXMLFile)) as loadXMLFile;
+
+		txtInstrucoes.text = loadXMLFile.interface_titulo[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void alterarIdioma (string idioma){
+		PlayerPrefs.SetString("idioma",idioma);
+		loadXMLFile.loadXMLData();
+
+		txtInstrucoes.text = loadXMLFile.interface_titulo[0];
+	}
+}
---
+++ Estudo Modulo 2/Assets/XML/loadXMLFile.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+
+
+public class loadXMLFile : MonoBehaviour {
+
+	public string arquivoXML; //nome do arquivo a ser carregado
+	public List<string>  interface_titulo;
+	public List<string>  interface_loja;
+
+	public string	idioma; //padrão é pt-br, setado na interface do Unity
+	// Use this for initialization
+	void Awake() {
+		loadXMLData();
+	}
+	public void loadXMLData(){
+
+		if(PlayerPrefs.GetString("idioma") != "")
+		{
+			idioma = PlayerPrefs.GetString("idioma");
+		}
+		
+		interface_titulo.Clear();
+		interface_loja.Clear();
+
+		TextAsset xmlData = (TextAsset) Resources.Load(idioma + "/" + arquivoXML); //Aponta para o Arquivo XML que será lido
+	
+		XmlDocument xmlDocument = new XmlDocument(); // Construtor
+
+		xmlDocument.LoadXml(xmlData.text);
+
+		foreach (XmlNode node in xmlDocument["language"].ChildNodes)
+		{
+			string nodeName = node.Attributes["name"].Value;
+			
+			foreach (XmlNode n in node["textos"].ChildNodes)
+			{
+				switch(nodeName)
+				{
+					case "interface_titulo":
+						interface_titulo.Add(n.InnerText);
+					break;
+					case "interface_loja":
+						interface_loja.Add(n.InnerText);
+					break;
+				}
+			}	
+
+		}
+
+	}
+	
+}
