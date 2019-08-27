52f4e2045e8fe808c9abdf2b6714a4dda86747aa
nisiybr <guilherme.nisiyama@gmail.com>
2019-08-26 22:32:48 -0300

    Criação de Modelo de Salas

---
+++ RPG Tiny Dungeon/Assets/Scripts/doorController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorController : MonoBehaviour {
+
+	public Transform saida;
+	public Transform posicaoCamera;
+
+}
