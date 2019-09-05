519f5c50c33053d3b168b72d877f256663b29817
nisiybr <guilherme.nisiyama@gmail.com>
2019-09-05 00:50:33 -0300

    Portas e Chaves

--- RPG Tiny Dungeon/Assets/Scripts/doorController.cs
+++ RPG Tiny Dungeon/Assets/Scripts/doorController.cs
@@ -8,4 +8,34 @@ public class doorController : MonoBehaviour {
 	public Transform posicaoCamera;
 	public int idSala;
 
+	public bool doorClosed;
+	public bool doorLocked;
+
+	public int idKey;
+	
+	private SpriteRenderer srPorta;
+
+	public Sprite portaTrancada;
+	public Sprite portaFechada;
+	public Sprite portaAberta;
+
+	private void Start() {
+		srPorta = GetComponent<SpriteRenderer>();
+		atualizaSprite();
+	}
+	public void atualizaSprite()
+	{
+		if(doorLocked)
+		{
+			srPorta.sprite = portaTrancada;
+		}
+		else if(doorClosed)
+		{
+			srPorta.sprite = portaFechada;
+		}
+		else
+		{
+			srPorta.sprite = portaAberta;
+		}
+	}
 }
