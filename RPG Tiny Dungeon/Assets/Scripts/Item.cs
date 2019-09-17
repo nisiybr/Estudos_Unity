6255709df61d73cf99135fa6b5c98dc49e3646d5
nisiybr <guilherme.nisiyama@gmail.com>
2019-09-17 20:31:19 -0300

    Luz e Sombras

---
+++ RPG Tiny Dungeon/Assets/Scripts/Item.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "new item",menuName = "Inventario/Item")]
+
+public class Item : ScriptableObject {
+	public int id = 0; 
+	public string name = "Novo Item";
+	public Sprite icon = null;
+
+}
